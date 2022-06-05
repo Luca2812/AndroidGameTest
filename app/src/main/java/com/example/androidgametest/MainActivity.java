d28015a1b29439cedf0838bcfbfc4b4264e497f5
Luca2812 <luca.strobl321@gmail.com>
2022-06-05 18:08:09 +0200

    game loop

--- app/src/main/java/com/example/androidgametest/MainActivity.java
+++ app/src/main/java/com/example/androidgametest/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.androidgametest;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // set to fullscreen
+        Window window=getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        // set content view to game
+        setContentView(new Game(this));
     }
 }
